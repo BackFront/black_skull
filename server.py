863491a131574170b932b9fe39c733fe392bae7a
Douglas Alves <alves.douglaz@gmail.com>
2016-12-01 16:20:22 -0200

    init server

---
+++ server.py
@@ -0,0 +1,19 @@
+import socket
+import sys
+
+"""
++=================== STEPS ===================+
+|                                             |
+|   1) Build sock conection                   |
+|   2) Receive connections                    |
+|   3) Store the connection                   |
+|   4) List the connection                    |
+|   5) Select a connection                    |
+|   6) Create a thread and Stabilizes contact |
+|       with this connection                  |
+|   7) Send commands to this connection       |
+|   8) Close conection                        |
+|   9) Kill thread                            |
+|                                             |
++---------------------------------------------+
+"""
